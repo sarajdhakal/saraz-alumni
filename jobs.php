f28f68ee6e600541a7f39a80caa19ce8ffcf062a
Saraj Dhakal <83003378+sarajdhakal@users.noreply.github.com>
2023-07-13 23:17:59 +0545

    Update jobs.php

--- jobs.php
+++ jobs.php
@@ -51,137 +51,61 @@
           </div> <!-- End Course Item-->
           
          
-          <div class="col-lg-4 col-md-6 d-flex align-items-stretch mt-4 mt-lg-0 h-50 d-inline-block">
-            <div id="foraa">
-            <div class="course-item">
-              <div class="course-content">
-                <span id="boot-icon" class="bi bi-briefcase" style="font-size:5rem"></span>
-                <a href="#" id="show-form"> <h4>Add New Job</h4></a>
+          <div class="col-lg-4 col-md-6 d-flex align-items-stretch text-center">
+            <div class="member">
+              <span id="boot-icon" class="bi bi-briefcase" style="font-size:5rem"></span>
+              <div class="member-content">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Offer A Job</button>                
               </div>
             </div>
-          </div> <!-- End Course Item-->
           </div>
-
-          <div class="container1" id="my-modal">
-            <h1 class="form-title">Offer a Job 
-                <span class="close">&times;</span>
-              </h1>
-            <form action="#">
-                <div class="main-user-info">
-                    <div class="user-input-box">
-                        <label for="fullName"> Job Title</label>
-                        <input type="text" id="fullName" name="fullName" placeholder="Job Title" required />
-                    </div>
-                    <div class="user-input-box">
-                        <label for="username">Organization</label>
-                        <input type="text" id="username" name="username" placeholder="Organizer?" required />
-                    </div>
-                    <div class="user-input-box">
-                        <label for="email">Alumni Name</label>
-                        <input type="email" id="email" name="email" placeholder="Name" required />
-                    </div>
-                    <div class="user-input-box">
-                        <label for="phoneNumber">Due Date</label>
-                        <input type="date" id="phoneNumber" name="phoneNumber" required />
-                    </div>
-                    <div class="user-input-box">
-                        <label for="textarea">Description of Job:</label>
-                          <textarea id="w3review" name="" placeholder="Write a short description of Job" rows="4" cols="50" required></textarea>
-                    </div>
-                    <div class="user-input-box">
-                      <label for="textarea"> How to Apply?</label>
-                        <textarea id="w3review" name="" placeholder="How to apply for Job?" rows="4" cols="50" required></textarea>
-                  </div>
-                  <div class="user-input-box">
-                    <label for="imageInput">Bannner of your Event:</label>
-                   <input type="file" id="imageInput" name="imageInput" accept="image/*">
-                   <input type="submit" value="Upload">
-               </div>
-                </div>
-                <div class="form-submit-btn">
-                    <input type="submit" value="Submit">
-                </div>
-            </form>
-        </div>
-
           </div>
-
         </div>
     </section><!-- End Courses Section -->
 
+    <div class="modal fade-out" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Offer A Job</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <form class="row g-3">
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Job Title</label>
+    <input type="text" class="form-control" id="inputEmail4" placeholder="Job Title">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Organization</label>
+    <input type="text" class="form-control" id="inputPassword4" placeholder="Organization" required>
+  </div>
+  <div class="col-md-6">
+    <label for="inputEmail4" class="form-label">Alumni Name</label>
+    <input type="text" class="form-control" id="inputEmail4" placeholder="Alumni Name">
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Due Date</label>
+    <input type="date" class="form-control" id="inputPassword4" required>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress" class="form-label">Description</label>
+    <textarea class="form-control" rows="4" cols="50" placeholder="Description of Job"></textarea>
+  </div>
+  <div class="col-12">
+    <label for="inputAddress2" class="form-label">Jobs Photo(Image Only)</label>
+    <input type="file" class="form-control" id="inputimage" name="banner" accept="image/*">
+  </div>
+  </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Alumni Management System</h3>
-            <p> Bharatpur- Chitwan <br><br>
-              <strong>Phone:</strong>#<br>
-              <strong>Email:</strong> hellohi@gmail.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="events.php">Events</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>Saraz</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
-          Designed by <a href="#">Saraz Coorporation</a>
-        </div>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <script>
-  
-  document.querySelector("#modal-btn").addEventListener("click", function(){
-
-document.querySelector(".container1").classList.add("active");
-
-document.querySelector(".container1 .close- btn").addEventListener("click", function(){
-   document.querySelector(".container1").classList.remove("active"); });
-
-});
-
-
-  </script>
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+ <?php include('head/footer.php') ?>
