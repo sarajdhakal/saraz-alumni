28c144c3e8c880792bb4f88a4e4f48d376a83e9e
Saraj Dhakal <83003378+sarajdhakal@users.noreply.github.com>
2023-07-15 19:09:31 +0545

    Update events.php

--- events.php
+++ events.php
@@ -1,4 +1,10 @@
-<?php include 'head/header.php' ?>
+
+
+<?php 
+session_start();
+include 'head/header.php';
+
+?>
 
 
   <main id="main">
@@ -25,7 +31,14 @@
                 <h5 class="card-title"><a href="events-details.php">Introduction to webdesign</a></h5>
                 <p class="fst-italic text-center">Sunday, September 26th at 7:00 pm</p>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat</p>
-                <a href="events-details.php"> <h4>View Details</h4></a>
+                <?php 
+                 if(!isset($_SESSION['is_login'])){?>
+                   <a href="login.php"><h4>View Details</h4></a>
+                <?php }
+                 else
+                 {?>   
+                   <a href="events-details.php"><h4>View Details</h4></a>
+                 <?php } ?>
               </div>
             </div>
           </div>
@@ -38,24 +51,33 @@
                 <h5 class="card-title"><a href="events-details.php">Marketing Strategies</a></h5>
                 <p class="fst-italic text-center">Sunday, November 15th at 7:00 pm</p>
                 <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo</p>
-                <a href="events-details.php"> <h4>View Details</h4></a>
+                 <?php 
+                 if(!isset($_SESSION['is_login'])){?>
+                   <a href="login.php"><h4>View Details</h4></a>
+                <?php }
+                 else
+                 {?>   
+                   <a href="events-details.php"><h4>View Details</h4></a>
+                 <?php } ?>
                 </div>
             </div>
           </div>
 
-            
-                    <div class="col-lg-4 col-md-6 d-flex align-items-stretch text-center">
-            <div class="member">
-              <span id="boot-icon" class="bi bi-calendar-event" style="font-size:5rem"></span>
-              <div class="member-content">
-              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add New Event</button>                
-              </div>
+          <?php 
+                 if(isset($_SESSION['is_login'])){?>
+                 <div class="col-lg-4 col-md-6 d-flex align-items-stretch text-center">
+          <div class="member">
+            <span id="boot-icon" class="bi bi-calendar-event" style="font-size:5rem"></span>
+            <div class="member-content">
+              <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo">Add New Event</button>
             </div>
-                    </div>
+          </div>
+        </div>
+                <?php }?>
         </div>
 
       </div>
-    </section><!-- End Events Section -->>
+    </section><!-- End Events Section -->
 
     <div class="modal fade-out" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog">
@@ -67,20 +89,28 @@
       <div class="modal-body">
       <form action class="row g-3">
   <div class="col-md-6">
-  <form action="">
+  <form action="events.php" method="post">
     <label for="inputEmail4" class="form-label">Title</label>
     <input type="text" class="form-control" id="inputEmail4" placeholder="Title of Event" required>
   </div>
   <div class="col-md-6">
     <label for="inputPassword4" class="form-label">Organizer</label>
-    <input type="text" class="form-control" id="inputPassword4" placeholder="Organizer?" required>
+    <input type="text" class="form-control" id="inputPassword4" placeholder="Organizer" required>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Email of Organizer</label>
+    <input type="email" class="form-control" id="inputPassword4" placeholder="Students or Alumni Email" required>
+  </div>
+  <div class="col-md-6">
+    <label for="inputPassword4" class="form-label">Type</label>
+    <input type="text" class="form-control" id="inputPassword4" placeholder="Eg: Cultural Event" required>
   </div>
   <div class="col-md-6">
     <label for="inputEmail4" class="form-label">Venue</label>
     <input type="text" class="form-control" id="inputEmail4" placeholder="Venue" required>
   </div>
   <div class="col-md-6">
-    <label for="inputPassword4" class="form-label">Date</label>
+    <label for="inputPassword4" class="form-label">Date and Time</label>
     <input type="datetime-local" class="form-control" id="inputPassword4" placeholder="Organizer?" required>
   </div>
   <div class="col-12">
@@ -94,9 +124,8 @@
        </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">ADD</button>
+        <button type="submit" class="btn btn-primary">ADD</button>
       </div>
-</form>
 </form>
     </div>
   </div>
