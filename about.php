b3ec336a4f0c5c362bdb979c90c218abf5cf88a5
Saraj Dhakal <83003378+sarajdhakal@users.noreply.github.com>
2023-07-15 19:07:55 +0545

    Updated about.php

--- about.php
+++ about.php
@@ -1,4 +1,10 @@
-<?php include 'head/header.php' ?>
+<?php include 'head/header.php';
+include 'config/db.php';
+$sql = "SELECT * FROM testimonials ";
+$result = $conn->query($sql);
+
+
+?>
 
 
   <main id="main">
@@ -46,55 +52,27 @@
 
         <div class="testimonials-slider swiper" data-aos="fade-up" data-aos-delay="100">
           <div class="swiper-wrapper">
-
+<?php
+          if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()){ ?>
             <div class="swiper-slide">
               <div class="testimonial-wrap">
                 <div class="testimonial-item">
-                  <img src="assets/img/testimonials/testimonials-1.jpg" class="testimonial-img" alt="">
-                  <h3>Saraj Dhakal</h3>
-                  <h4>Ceo &amp; Labour</h4>
+                  <img src="assets/img/2.png" class="testimonial-img" alt="">
+                  <h3><?= $row["testimonials_name"] ?></h3>
+                  <h4><?= $row["post"] ?></h4>
                   <p>
                     <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    The AMS made organizing our alumni reunion a breeze! The event management features allowed us to effortlessly 
-                    coordinate logistics and reach out to all attendees, resulting in a successful and unforgettable gathering.
+                    <?= $row["description"] ?>
                     <i class="bx bxs-quote-alt-right quote-icon-right"></i>
                   </p>
                 </div>
               </div>
             </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-wrap">
-                <div class="testimonial-item">
-                  <img src="assets/img/testimonials/testimonials-2.jpg" class="testimonial-img" alt="">
-                  <h3>Soviyat Lamsal</h3>
-                  <h4>Founder</h4>
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    Thanks to the Alumni Management System, I was able to reconnect with old classmates and expand my professional network, 
-                    leading to exciting job opportunities I wouldn't have found elsewhere.
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                </div>
-              </div>
-            </div><!-- End testimonial item -->
-
-            <div class="swiper-slide">
-              <div class="testimonial-wrap">
-                <div class="testimonial-item">
-                  <img src="assets/img/testimonials/testimonials-3.jpg" class="testimonial-img" alt="">
-                  <h3>Nishani Kumari Rai</h3>
-                  <h4>Founder</h4>
-                  <p>
-                    <i class="bx bxs-quote-alt-left quote-icon-left"></i>
-                    I highly recommend the Alumni Management System to fellow graduates. 
-                    Through the platform, I received valuable mentorship and career guidance, which played a crucial role in my professional development and success
-                    <i class="bx bxs-quote-alt-right quote-icon-right"></i>
-                  </p>
-                </div>
-              </div>
-            </div><!-- End testimonial item -->
-            <div class="swiper-slide">
+  <?php  }
+  }
+        ?>  
+        <div class="swiper-slide">
               <div class="testimonial-wrap">
                 <div class="testimonial-item">
                   <img src="assets/img/testimonials/testimonials-4.jpg" class="testimonial-img" alt="">
@@ -118,64 +96,4 @@
 
   </main><!-- End #main -->
 
-  <!-- ======= Footer ======= -->
-  <footer id="footer">
-
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6 footer-contact">
-            <h3>Alumni Management System</h3>
-            <p> Bharatpur- Chitwan <br><br>
-              <strong>Phone:</strong>#<br>
-              <strong>Email:</strong> hellohi@gmail.com<br>
-            </p>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="about.php">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="events.php">Events</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container d-md-flex py-4">
-
-      <div class="me-md-auto text-center text-md-start">
-        <div class="copyright">
-          &copy; Copyright <strong><span>Saraz</span></strong>. All Rights Reserved
-        </div>
-        <div class="credits">
-          <!-- All the links in the footer should remain intact. -->
-          <!-- You can delete the links only if you purchased the pro version. -->
-          <!-- Licensing information: https://bootstrapmade.com/license/ -->
-          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/mentor-free-education-bootstrap-theme/ -->
-          Designed by <a href="#">Saraz Coorporation</a>
-        </div>
-      </div>
-    </div>
-  </footer><!-- End Footer -->
-
-  <div id="preloader"></div>
-  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-
-  <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
-  <script src="assets/vendor/aos/aos.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/php-email-form/validate.js"></script>
-
-  <!-- Template Main JS File -->
-  <script src="assets/js/main.js"></script>
-
-</body>
-
-</html>
+  <?php include('head/footer.php') ?>
