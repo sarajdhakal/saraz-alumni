e6dd359eecd167b8ed9bb291117416e2d9e6d2b4
Saraj Dhakal <83003378+sarajdhakal@users.noreply.github.com>
2023-09-15 18:01:41 +0545

    Update header.php

--- head/header.php
+++ head/header.php
@@ -13,7 +13,7 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title> AMS -INDEX</title>
+  <title> Alumni Management System</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
