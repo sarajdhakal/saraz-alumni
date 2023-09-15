8004df7c459c85877af7ab62734407b76bd21ca9
Saraj Dhakal <83003378+sarajdhakal@users.noreply.github.com>
2023-09-15 18:12:51 +0545

    Update alumni.php

--- alumni.php
+++ alumni.php
@@ -4,80 +4,21 @@ session_start();
 if(!isset($_SESSION['is_login']) && $_SESSION == false){
      header('Location:login.php');
 }
+include 'head/header.php';
+include 'config/db.php';
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-
 <head>
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
-  <title> AMS -INDEX</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  <link href="assets/img/2.png" rel="icon">
-
-  <!-- Google Fonts -->
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-
-  <!-- Vendor CSS Files -->
-  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
-  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!-- Template Main CSS File -->
-  <link href="assets/css/style.css" rel="stylesheet">
   <link href="assets/css/alumni.css" rel="stylesheet">
   
 </head>
-<body>
-
-   <!-- ======= Header ======= -->
-   <header id="header" class="fixed-top">
-    <div class="container d-flex align-items-center">
- <!-- Uncomment below if you prefer to use an image logo -->
- <!-- <a href="index.php" class="logo me-auto"><img src="assets/img/2.png" alt="" class="img-fluid"></a> -->
-      <h1 class="logo me-auto"><a href="index.php" style="text-decoration: none;">Alumni Management System</a></h1>
-
-      <nav id="navbar" class="navbar order-last order-lg-0">
-        <ul>
-          <li><a  <?php if (strpos($_SERVER['PHP_SELF'], 'index.php')) echo 'class="active"';?> href="index.php"  >Home</a></li>
-          <li><a  <?php if (strpos($_SERVER['PHP_SELF'], 'profile.php')) echo 'class="active"';?> href="profile.php"   >Profile</a></li>
-          <li><a  <?php if (strpos($_SERVER['PHP_SELF'], 'about.php')) echo 'class="active"';?> href="about.php"  >About</a></li>
-          <li><a <?php if (strpos($_SERVER['PHP_SELF'], 'jobs.php')) echo 'class="active"';?> href="jobs.php" >Jobs</a></li>
-          <li><a <?php if (strpos($_SERVER['PHP_SELF'], 'alumni.php')) echo 'class="active"';?> href="alumni.php"  >Alumni</a></li>
-          <li><a <?php if (strpos($_SERVER['PHP_SELF'], 'events.php')) echo 'class="active"';?>  href="events.php">Events</a></li>
-                 
-          
-          <?php 
-if(!isset($_SESSION['is_login'])){?>
-   <li><a  <?php if (strpos($_SERVER['PHP_SELF'], 'login.php')) echo 'class="active"';?>href="login.php">Sign In</a></li> 
-   <li><a  <?php if (strpos($_SERVER['PHP_SELF'], 'register.php')) echo 'class="active"';?>href="register.php">Sign Up</a></li> 
- <?php }
-else
-{?>   
- <li><a href="logout.php">Sign Out</a></li> 
-<?php } ?>
-        </ul>
-        <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
-    </div>
-  </header><!-- End Header -->
-
-
   <main id="main" data-aos="fade-in">
 
     <!-- ======= Breadcrumbs ======= -->
     <div class="breadcrumbs">
       <div class="container">
         <h2>Alumni</h2>
-        <p>Est dolorum ut non facere possimus quibusdam eligendi voluptatem. Quia id aut similique quia voluptas sit quaerat debitis. Rerum omnis ipsam aperiam consequatur laboriosam nemo harum praesentium. </p>
+        <p> </p>
       </div>
     </div><!-- End Breadcrumbs -->
 
